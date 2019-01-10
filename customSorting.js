7ee2aad6ab0447052aa027d86e72c5a33afca2de
XiaoyongXu <xxy1994212@gmail.com>
2019-01-10 19:51:40 +0000

    finish the customSorting

--- customSorting.js
+++ customSorting.js
@@ -1,5 +1,28 @@
 var array = [10, 2, 5, 1, 9];
 array.sort(function(a,b){
-    return a - b;
+  return a - b;
 });
 console.log(array);
+
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+students.sort(function(a,b){
+  var nameA = a.name.toUpperCase();
+  var nameB = b.name.toUpperCase();
+  if (nameA < nameB){
+    return -1;
+  } else if (nameA > nameB){
+    return 1;
+  } else {
+    var ageA = a.age;
+    var ageB = b.age;
+    return -(ageA - ageB);
+  }
+});
+
+console.log(students);
