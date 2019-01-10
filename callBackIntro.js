379527452d2920a6e44f3edba5da0b282505a5e3
XiaoyongXu <xxy1994212@gmail.com>
2019-01-10 17:38:42 +0000

    Print the index of Waldo when found

--- callBackIntro.js
+++ callBackIntro.js
@@ -2,13 +2,14 @@
 function findWaldo(arr, found) {
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] === "Waldo") {
-      found();   // execute callback
+      
+      found(i);   // execute callback
     }
   }
 }
 
-function actionWhenFound() {
-  console.log("Found him!");
+function actionWhenFound(number) {
+  console.log("Found Waldo at index "+ number);
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
