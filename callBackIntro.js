f5941cea1cb1deb5ea9bd7979fa90096d70bcbc3
XiaoyongXu <xxy1994212@gmail.com>
2019-01-10 17:53:46 +0000

    Refactor the for loop with arrayForEach function

--- callBackIntro.js
+++ callBackIntro.js
@@ -1,11 +1,18 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      
-      found(i);   // execute callback
+  var i = 0;
+  arr.forEach(function(element) {
+    if (element === "Waldo"){
+      found(i);
     }
-  }
+    i ++;
+  });
+  // for (var i = 0; i < arr.length; i++) {
+  //   if (arr[i] === "Waldo") {
+      
+  //     found(i);   // execute callback
+  //   }
+  // }
 }
 
 function actionWhenFound(number) {
