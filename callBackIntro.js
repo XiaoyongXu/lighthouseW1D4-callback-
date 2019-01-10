004bb97ef5eea9c336d42036d8715b53cea08445
XiaoyongXu <xxy1994212@gmail.com>
2019-01-10 20:05:56 +0000

    refactor for cleaner coding

--- callBackIntro.js
+++ callBackIntro.js
@@ -1,19 +1,10 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  // var i = 0;
   arr.forEach(function(element) {
     if (element === "Waldo"){
-      // found(i);
       found(arr.indexOf(element));
     }
-    // i ++;
   });
-  // for (var i = 0; i < arr.length; i++) {
-  //   if (arr[i] === "Waldo") {
-      
-  //     found(i);   // execute callback
-  //   }
-  // }
 }
 
 function actionWhenFound(number) {
